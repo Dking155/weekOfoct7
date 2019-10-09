393a976ec7487827583a98378817d67171cb81e7
100134969 <kingpatrick455@gmail.com>
2019-10-09 08:37:41 -0500

    pushing 10-9 2nd version comments

--- NewWork.py
+++ NewWork.py
@@ -61,3 +61,8 @@ months = "jan\nfeb\nmar\napr\nmay\njun\njuly\naug"
 print("here are the days: ", days)
 print("here are the months: ", months)
 
+print("""
+Theres something going on here.
+With the three double-quotes.
+Well be able to type as much as we like
+even 4 lines if we want, or 5 or 6.""")
